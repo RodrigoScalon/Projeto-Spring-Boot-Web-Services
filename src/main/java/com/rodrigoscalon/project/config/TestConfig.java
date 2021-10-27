08b5f19fb1025e097bc217e52da53591c1aabc5e
Rodrigo Scalon <rodrigoscalon@gmail.com>
2021-10-27 19:08:08 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/rodrigoscalon/project/config/TestConfig.java
+++ src/main/java/com/rodrigoscalon/project/config/TestConfig.java
@@ -51,6 +51,18 @@ public class TestConfig implements CommandLineRunner {
 		
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
+		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		
+		
 		User u1 = new User(null, "Bruce Wayne", "bruce@gmail.com", "988888888", "123456"); 
 		User u2 = new User(null, "Clark Kent", "clark@gmail.com", "977777777", "123456"); 
 		
