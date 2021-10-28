da473201dacf27bd42bbe5edf2d00e1b466d7e13
Rodrigo Scalon <rodrigoscalon@gmail.com>
2021-10-28 12:08:37 -0300

    Subtotal & Total methods

--- src/main/java/com/rodrigoscalon/project/entities/Order.java
+++ src/main/java/com/rodrigoscalon/project/entities/Order.java
@@ -44,6 +44,7 @@ public class Order implements Serializable {
 	@OneToOne (mappedBy = "order", cascade = CascadeType.ALL)
 	private Payment payment;
 	
+	
 	public Order() {
 
 	}
@@ -103,6 +104,15 @@ public class Order implements Serializable {
 
 	public Set<OrderItem> getItems(){
 		return items;
+	}
+	
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
 	}
 	
 	@Override
