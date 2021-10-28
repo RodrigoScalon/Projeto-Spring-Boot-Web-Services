7dbacd9a4bf7dcc9003c85e7132988ed2a38a1b4
Rodrigo Scalon <rodrigoscalon@gmail.com>
2021-10-28 13:36:31 -0300

    Exception handling - delete

--- src/main/java/com/rodrigoscalon/project/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/rodrigoscalon/project/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.rodrigoscalon.project.services.exceptions.DatabaseException;
 import com.rodrigoscalon.project.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -23,5 +24,12 @@ public class ResourceExceptionHandler {
 		}
 	
 	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
 
+}
 }
--- src/main/java/com/rodrigoscalon/project/services/UserService.java
+++ src/main/java/com/rodrigoscalon/project/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.rodrigoscalon.project.entities.User;
 import com.rodrigoscalon.project.repositories.UserRepository;
+import com.rodrigoscalon.project.services.exceptions.DatabaseException;
 import com.rodrigoscalon.project.services.exceptions.ResourceNotFoundException;
 
 
@@ -36,7 +39,14 @@ public class UserService {
 	}
 	
 	public void delete (Long id) {
+		try {
 		repository.deleteById(id);
+	} catch (EmptyResultDataAccessException e) {
+		throw new ResourceNotFoundException(id);
+	} catch (DataIntegrityViolationException e) {
+		throw new DatabaseException(e.getMessage());
+		
+	}
 	}
 	
 	public User update (Long id, User obj) {
