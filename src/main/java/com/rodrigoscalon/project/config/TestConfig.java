e2b5f9e7e0ad2295fb429e5024ff28722c807827
Rodrigo Scalon <rodrigoscalon@gmail.com>
2021-10-28 11:32:37 -0300

    Payment, one-to-one association

--- src/main/java/com/rodrigoscalon/project/config/TestConfig.java
+++ src/main/java/com/rodrigoscalon/project/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.rodrigoscalon.project.entities.Category;
 import com.rodrigoscalon.project.entities.Order;
 import com.rodrigoscalon.project.entities.OrderItem;
+import com.rodrigoscalon.project.entities.Payment;
 import com.rodrigoscalon.project.entities.Product;
 import com.rodrigoscalon.project.entities.User;
 import com.rodrigoscalon.project.entities.enums.OrderStatus;
@@ -84,6 +85,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 		
 	} 
